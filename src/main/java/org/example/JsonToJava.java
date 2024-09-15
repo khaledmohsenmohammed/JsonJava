ebd44367b97be6c043ee2c6ca53908cf1d3ea9a6
Khaled Mohsen <khaledmohsenmohammed@gmail.com>
2024-09-15 14:49:08 +0300

    create json file for each row in DB coming from Query

--- src/main/java/org/example/JsonToJava.java
+++ src/main/java/org/example/JsonToJava.java
@@ -1,36 +1,54 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class JsonToJava {
-    public static void main(String [] args) throws ClassNotFoundException, SQLException {
-        CustomerDetails customerDetails = new CustomerDetails() ; //object from class Customer and set the data return from the DB
+    public static void main(String [] args) throws ClassNotFoundException, SQLException, IOException {
+        ArrayList <CustomerDetails> arrayListCustomerDetails =  new ArrayList<CustomerDetails>() ;
+
         // Load MySQL JDBC Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Dynamically get the project directory path
+        String projectPath = Paths.get("").toAbsolutePath().toString();
 
         // Correct JDBC URL: "jdbc:mysql://" instead of "jdbc.mysql://"
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business", "root", "toor");
 
         // Object of Statement class will help to execute queries
         Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM CustomerInfo WHERE Location = 'Asia' AND PurchasedDate = '2024-09-13' LIMIT 1 ;");
+        ResultSet rs = st.executeQuery("SELECT * FROM CustomerInfo WHERE Location = 'Asia' AND PurchasedDate = '2024-09-13';");
 
         // Processing the ResultSet
         while (rs.next()) {
-            //System.out.println(rs.getString(1)); // Assuming the first column is a String
+            CustomerDetails customerDetails = new CustomerDetails() ; //object from class Customer and set the data return from the DB
+
+            //3 diff json file , need  3 diff java object
+            // Assuming the first column is a String
             customerDetails.setCustomerName(rs.getString(1));
-            //System.out.println(rs.getString(2)); // Assuming the second column is a String
+           // Assuming the second column is a String
             customerDetails.setPurchasedData(rs.getString(2));
-            //System.out.println(rs.getInt(3));    // Assuming the third column is an int
+            // Assuming the third column is an int
             customerDetails.setAmount(rs.getInt(3));
-            //System.out.println(rs.getString(4)); // Assuming the fourth column is a String
+            // Assuming the fourth column is a String
             customerDetails.setLocation(rs.getString(4));
-
-            // print the result for the CustomerName & PurchasedData
-            System.out.println(customerDetails.getCustomerName());
-            System.out.println(customerDetails.getPurchasedData());
-
+            arrayListCustomerDetails.add(customerDetails);
         }
+        //create json file for each row in DB coming from Query
+        for(int i = 0 ;i<arrayListCustomerDetails.size();i++){
+            ObjectMapper objectMapper = new ObjectMapper() ;
+            // Define the file path dynamically using the project path
+            File file = new File(projectPath + "/customerInfo" + i + ".json");
+            // Write the object to the file
+            objectMapper.writeValue(file, arrayListCustomerDetails.get(i));
+        }
+
         // Close the connection
         conn.close();
 
